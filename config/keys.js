a1405b1c2c6de0c600ada85aa486bfd1e76cf40e
25sprout-alan <alan@25sprout.com>
2019-03-16 02:22:12 +0800

    feat: Connecting to mongodb with mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://alan:J122872769@ds139242.mlab.com:39242/25sprout'
+}
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB if success then mongoDB connect !!
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected !!'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
 
