7844dc9f4df4acdb8f2cd2c35faa2fcbbd8e722c
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2019-03-19 19:43:37 +0800

    feat: Add feature of create and update profile to profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,4 +28,58 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.status(404).json(errors))
 });
 
+// @route   POST api/profile
+// @desc    Create or Edit user profile
+// @access  Private
+
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.company) profileFields.company = req.body.company;
+    if(req.body.location) profileFields.location = req.body.location;
+    if(req.body.bio) profileFields.bio = req.body.bio;
+    if(req.body.status) profileFields.status = req.body.status;
+    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    // Skills spilt into array
+    if(typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',') // '1,2,3,4' => [1,2,3,4]
+    }
+
+    // Soical
+    profileFields.soical = {};
+    if(req.body.youtube) profileFields.soical.youtube = req.body.youtube;
+    if(req.body.twitter) profileFields.soical.twitter = req.body.twitter;
+    if(req.body.facebook) profileFields.soical.facebook = req.body.facebook;
+    if(req.body.linkedin) profileFields.soical.linkedin = req.body.linkedin;
+    if(req.body.instagram) profileFields.soical.instagram = req.body.instagram;
+
+    // 找 logged 的 人
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if(profile) {
+                // If there is profile that means we want to update profile(profileFields是一個大物件)
+                Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                ).then(profile => res.json(profile));
+            } else {
+                // Create new profile
+
+                // Check if handle exist (handle is for SEO  and to access the profile page)
+                Profile.findOne({ handle: profileFields.handle })
+                    .then(profile => {
+                        if(profile) {
+                            errors.handle = 'Handle 已經存在'
+                            res.status(400).json(errors)
+                        }
+
+                        // new Profile
+                        new Profile(profileFields).save().then(profile => res.json(profile));
+                    });
+            } 
+        }) 
+});
+
 module.exports = router
