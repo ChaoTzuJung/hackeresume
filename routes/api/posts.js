a709c6aeba2175f612cfee00fd220c0f3702349d
25sprout-alan <alan@25sprout.com>
2019-03-22 00:31:03 +0800

    feat: feat: Add post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route   GET api/post/test
-// @desc    Tests post route
-// @access  Public 
-router.get('/test', (req, res) => res.json({msg: 'Posts work!'}));
+const Post = require('../../models/Post');
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private 
+router.post('/', passport.authenticate('jwt', { session: fasle }), (req, res) => {
+    // 建立collection的第一份資料
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.body.id
+    })
+    // post 參數應該就是 newPost 物件
+    newPost.save().then(post => res.json(post));
+});
 
 module.exports = router
