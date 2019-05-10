772ba44fcae66a21c0330422200f19970bf73b79
25sprout-alan <alan@25sprout.com>
2019-05-11 00:08:56 +0800

    feat: Add profile reducer and action

---
+++ client/src/actions/profile.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { PROFILE_LOADING, GET_PROFILE } from '../actions/types';
+
+export const getCurrentProfile = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get('/api/profile')
+        .then(res => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        .catch(err => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: {}
+            })
+        )
+    );
+}
+
+export const setProfileLoading = () => ({
+    type: PROFILE_LOADING,
+});
