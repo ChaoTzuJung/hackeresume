84e6b3c061eb93cc36de24269c4b5c3106645f2e
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2019-05-15 16:50:28 +0800

    fix: format time data

--- client/src/components/Experience/Experience.js
+++ client/src/components/Experience/Experience.js
@@ -3,6 +3,8 @@ import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import Moment from 'react-moment';
+
 import { deleteExperience } from '../../actions/profile';
 
 class Experience extends Component {
@@ -11,7 +13,10 @@ class Experience extends Component {
             <tr key={exp._id}>
                 <td>{exp.company}</td>
                 <td>{exp.title}</td>
-                <td>{exp.from} - {exp.to}</td>
+                <td>
+                    <Moment format="YYYY/MM/DD">{exp.from}</Moment> - {' '}
+                    {exp.to === null ? 'Now' : <Moment format="YYYY/MM/DD">{exp.to}</Moment>}
+                </td>
                 <td><button className="btn btn-danger">Delete</button></td>
             </tr>
         ));
